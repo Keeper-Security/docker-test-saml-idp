810714adfd01937f7fcde9322fde2f44a2392fb1
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-08-10 14:50:32 -0400

    Fix HTTPS issue

--- config/simplesamlphp/saml20-sp-remote.php
+++ config/simplesamlphp/saml20-sp-remote.php
@@ -12,6 +12,13 @@ $metadata['http://localhost:1511/'] = array(
     'simplesaml.attributes'      => true
 );
 
+$metadata['https://localhost:1511/'] = array(
+    'AssertionConsumerService' => 'https://localhost:1511/authentication/check_login',
+    'NameIDFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+    'simplesaml.nameidattribute' => 'uid',
+    'simplesaml.attributes'      => true
+);
+
 $metadata['http://localhost/'] = array(
     'AssertionConsumerService' => 'http://localhost/authentication/check_login',
     'NameIDFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
